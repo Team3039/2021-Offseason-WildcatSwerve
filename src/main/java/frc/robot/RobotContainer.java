4dec0912dc8d3d4b3c9a445734c16bded9324c98
Bodhaansh Ravipati <45471123+BodhaanshRavipati325@users.noreply.github.com>
2021-10-12 20:07:27 -0500

    -Init Commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,81 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.Button;
+import frc.robot.controllers.PS4Gamepad;
+import frc.robot.subsystems.DrivetrainSubsystem;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final DrivetrainSubsystem m_drivetrainSubsystem = new DrivetrainSubsystem();
+
+  private final PS4Gamepad m_controller = new PS4Gamepad(0);
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    m_drivetrainSubsystem.setDefaultCommand(new FunctionalCommand(
+            () -> { // Do nothing on initialize
+            },
+            () -> m_drivetrainSubsystem.drive(
+                    ChassisSpeeds.fromFieldRelativeSpeeds(
+                            m_controller.getLeftYAxis(),
+                            m_controller.getLeftXAxis(),
+                            m_controller.getRightXAxis(),
+                            m_drivetrainSubsystem.getGyroscopeRotation()
+                    )
+            ),
+            interrupted -> {
+              // Stop the drivetrain
+              m_drivetrainSubsystem.drive(new ChassisSpeeds(0.0, 0.0, 0.0));
+            },
+            () -> false, // Command never finishes
+            m_drivetrainSubsystem
+    ));
+
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    // Back button zeros the gyroscope
+    
+    Button driverPadButton = m_controller.getButtonPad();
+
+    driverPadButton.whenPressed(m_drivetrainSubsystem::zeroGyroscope);
+    // new Button(m_controller::getButtonPad)
+    //         // No requirements because we don't need to interrupt anything
+    //         .whenPressed(m_drivetrainSubsystem::zeroGyroscope);
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return new InstantCommand();
+  }
+}
