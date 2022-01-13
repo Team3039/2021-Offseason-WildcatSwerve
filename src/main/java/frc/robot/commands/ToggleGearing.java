d44f6883de83cb900665191a7f9a83a63829729b
Bodhaansh Ravipati <45471123+BodhaanshRavipati325@users.noreply.github.com>
2022-01-13 11:30:52 -0600

    Update and rename SetHighGear.java to ToggleGearing.java

--- src/main/java/frc/robot/commands/ToggleGearing.java
+++ src/main/java/frc/robot/commands/ToggleGearing.java
@@ -7,10 +7,10 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drive;
 
-public class SetHighGear extends CommandBase {
+public class ToggleGearing extends CommandBase {
   /** Creates a new SetHighGear. */
   boolean isHighGear;
-  public SetHighGear(boolean isHighGear) {
+  public ToggleGearing(boolean isHighGear) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.isHighGear = isHighGear;
   }
